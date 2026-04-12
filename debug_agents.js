41f51ab40ceebe5701e3d5c7ad17a73368f89e80
blaqjeff <prosperjeffrey141@gmail.com>
2026-04-12 06:11:57 +0100

    Remove internal docs and local artifacts from tracking

--- debug_agents.js
+++
@@ -1,7 +0,0 @@
-import fetch from 'node-fetch';
-async function run() {
-  const res = await fetch('http://localhost:3001/api/agents');
-  const data = await res.json();
-  console.log(JSON.stringify(data, null, 2));
-}
-run();
